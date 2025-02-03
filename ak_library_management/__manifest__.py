e6db97596ed2d06fe9432d65f4006cf0460ccda7
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-02-03 18:51:24 +0530

    Add files via upload

---
+++ ak_library_management/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Library Management',
+    'version': '18.0.1.0.0',
+    'category': 'Library',
+    'author': 'Vishant Bhavsar',
+    'website': 'http://aktivesoftware.com',
+    'depends': ['base', 'web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/library_book_views.xml',
+        'views/library_member_views.xml',
+        'views/library_book_category_views.xml',
+
+        'views/menu.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+}
