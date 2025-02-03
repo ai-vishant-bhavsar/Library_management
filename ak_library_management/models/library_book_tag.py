b6ae13e86c65ea94721bf82f83a438af092355b5
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-02-03 18:56:25 +0530

    Add files via upload

---
+++ ak_library_management/models/library_book_tag.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class LibraryBookTags(models.Model):
+    _name = 'library.book.tags'
+    _description = 'Book Tags'
+
+    name = fields.Char(string="Tag Name", required=True)
