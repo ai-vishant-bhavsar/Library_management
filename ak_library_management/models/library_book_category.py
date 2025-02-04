5a5fc2cfb4fb7d6e1845f3a27bd2daca48597eaa
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-02-04 18:56:42 +0530

    Delete ak_library_management directory

--- ak_library_management/models/library_book_category.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class LibraryBookCategory(models.Model):
-    _name = 'library.book.category'
-    _description = 'Book Category'
-
-    name = fields.Char(string="Category Name", required=True)
-    tag_ids = fields.Many2many('library.book.tags', string="Tags")
--- ak_library_management/models/library_book_tag.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class LibraryBookTags(models.Model):
-    _name = 'library.book.tags'
-    _description = 'Book Tags'
-
-    name = fields.Char(string="Tag Name", required=True)
