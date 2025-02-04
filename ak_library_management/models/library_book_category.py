2e1b0941b629579eeb5f55bd62d39c6d9ee7207f
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-02-04 18:54:15 +0530

    Delete ak_library_management directory

--- ak_library_management/models/library_book_category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class LibraryBookCategory(models.Model):
-    _name = 'library.book.category'
-    _description = 'Book Category'
-
-    name = fields.Char(string="Category Name", required=True)
-    description = fields.Text(string="Description")
