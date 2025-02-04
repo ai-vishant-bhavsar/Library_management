e8ab9f6395ed203a97a33133cecb5491e182735e
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-02-04 14:24:32 +0530

    Update __manifest__.py

--- ak_library_management/__manifest__.py
+++ ak_library_management/__manifest__.py
@@ -5,6 +5,7 @@
     'category': 'Library',
     'author': 'Vishant Bhavsar',
     'website': 'http://aktivesoftware.com',
+    'license': 'LGPL-3'
     'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
