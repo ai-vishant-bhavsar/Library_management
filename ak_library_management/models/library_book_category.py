b575e391f0529c544f7e6c12e2ddcaed744d37e8
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-02-05 14:19:43 +0530

    Update library_book_category.py

--- ak_library_management/models/library_book_category.py
+++ ak_library_management/models/library_book_category.py
@@ -2,8 +2,7 @@
 from odoo import models, fields
 
 class LibraryBookCategory(models.Model):
-    """ This is a normal model, it is store
-        the categories for the books """
+    """ This model is store the categories for the books """
     _name = 'library.book.category'
     _description = 'Book Category'
 
