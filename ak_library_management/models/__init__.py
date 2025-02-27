5fe6c4971028df0591918bbb779355d72ff4e086
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-02-27 13:20:18 +0530

    Delete ak_library_management directory

--- ak_library_management/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import library_member
-from . import library_library
-from . import product_template
-from . import library_book_tag
-from . import library_book_category
-from . import library_bulk_upload_books
-from . import res_users
-from . import sale_order
