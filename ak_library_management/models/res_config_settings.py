a0192647313b678f6b4dec4779f0968dcfabb67e
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-03-26 10:33:29 +0530

    Delete ak_library_management directory

--- ak_library_management/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-    _description = 'Configration Settings'
-
-    borrow_limit = fields.Integer(string='Borrow Limit')
