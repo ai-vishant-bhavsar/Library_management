bddc3dc3d7c37e18dec9f6e5ff5d5417af71463d
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-03-26 10:28:57 +0530

    Update __manifest__.py

--- ak_library_management/__manifest__.py
+++ ak_library_management/__manifest__.py
@@ -2,7 +2,7 @@
 # This is an information file of the module
 {
     'name': 'Library Management',
-    'version': '18.0.1.0.0',
+    'version': '18.0.2.4.0',
     'category': 'Library',
     'author': 'Vishant Bhavsar',
     'website': 'http://aktivesoftware.com',
