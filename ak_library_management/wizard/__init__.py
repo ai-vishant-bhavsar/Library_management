4af66c6254659330c6b0f29e644d604d3dd163bc
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-03-27 19:12:03 +0530

    Delete ak_library_management directory

--- ak_library_management/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import borrow_transaction_history_wizard
--- ak_library_management/wizard/trust_worthy_or_not.py
+++
@@ -1,6 +0,0 @@
-from odoo import models
-
-
-class TrustWorthyOrNot(models.Model):
-    _inherit = 'borrow.transaction'
-    _description = 'Customer is trust worthy or not'
