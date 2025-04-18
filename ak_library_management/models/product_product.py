cc6b91018eb37827720759c3727f1c30936c595e
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-04-18 14:42:18 +0530

    Delete ak_library_management directory

--- ak_library_management/models/product_product.py
+++
@@ -1,9 +0,0 @@
-from odoo import models,fields
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    seller_ids= fields.One2many('product.supplierinfo', 'product_id', 'Vendors',
-                                 depends_context=('company',))
-    variant_seller_ids = fields.One2many('product.supplierinfo', 'product_id')
