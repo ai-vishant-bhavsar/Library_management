55014c4777116f30751623688e84adbabcc2cab6
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-04-23 05:54:30 +0530

    Update sale_order.py

--- vishant_crm_customisation/models/sale_order.py
+++ vishant_crm_customisation/models/sale_order.py
@@ -8,16 +8,8 @@ class SaleOrder(models.Model):
 
     job_name = fields.Char(string='Job Name')
 
-    @api.depends('name')
-    def action_confirm(self):
-        for order in self:
-            if order.opportunity_id:
-                order.job_name = order.opportunity_id.name
-            if not order.job_name:
-                continue
-        return super(SaleOrder, self).action_confirm()
-
     def _prepare_invoice(self):
+        """ This method is to prepare the invoice values and pass them to inovice """
         invoice_vals = super()._prepare_invoice()
         invoice_vals['job_name'] = f"{self.name} - {self.job_name}"
         return invoice_vals
