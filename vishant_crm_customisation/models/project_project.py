357f1a6cb345a4e8de542a234145156d7f9ba247
Vishant Bhavsar <vishant.bhavsar@icreativetechnolabs.com>
2025-04-23 05:53:23 +0530

    Update project_project.py

--- vishant_crm_customisation/models/project_project.py
+++ vishant_crm_customisation/models/project_project.py
@@ -6,4 +6,11 @@ class ProjectProject(models.Model):
     """ This model is inherited to add a new field named job_name"""
     _inherit = 'project.project'
 
-    job_name = fields.Char(string='Job Name')
+    job_name = fields.Char(string='Job Name', compute='_compute_job_from_sale_order', store='True')
+
+    
+    @api.depends('sale_order_id.job_name')
+    def _compute_job_from_sale_order(self):
+        """ This method is to compute the job name from the sale_order.job_name """
+        for rec in self:
+            rec.job_name = rec.sale_order_id.job_name
